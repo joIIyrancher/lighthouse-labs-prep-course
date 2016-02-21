f7b79a16af77b50fc220079f4b8c8d91b9a040a9
Johnny Hsiao <johnnyhsiao87@gmail.com>
2016-02-20 21:08:20 -0500

    feature added: line-through items bought

--- recipe/script.js
+++ recipe/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
 	console.log("script.js is connected!");
-	$('img').click(
-		function() {
-			$(this).hide();
-		}
-	);
+	$('img').click(function() {
+		$(this).hide();
+	});
+
+	$('li.ingredient_items').click(function() {
+		$(this).toggleClass('item-bought');
+	});
+
+	$('div').hover(function() {
+		$(this).css("background-color", "#FFE4C4");
+	}, function() {
+		$(this).css("background-color", "#f5f5dc");
+	});
 });
